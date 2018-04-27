b5f35e55d193550d571467f88860c039c839b1d5
Jacobo Araujo <info@jacoboaraujo.com>
2018-04-27 17:32:27 +0200

    Solucionado error de react-router con webpack-dev-server

--- frontend/src/build/webpack.common.js
+++ frontend/src/build/webpack.common.js
@@ -23,7 +23,7 @@ module.exports = {
             loader: 'css-loader',
             options: {
               modules: true,
-              localIdentName: '[path][name]__[local]--[hash:base64:5]'
+              localIdentName: '[name]__[local]--[hash:base64:5]'
             }
           }
         ]
--- frontend/src/build/webpack.dev.js
+++ frontend/src/build/webpack.dev.js
@@ -7,7 +7,8 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './public',
-    hot: true
+    hot: true,
+    historyApiFallback: true,
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),

bf0d356d59b7cf9c26ff86dcb9b648f20109afa3
Jacobo Araujo <info@jacoboaraujo.com>
2019-03-10 13:06:29 +0100

    WIP

--- server/extras/models.py
+++ server/extras/models.py
@@ -2,10 +2,10 @@
 """Extra models."""
 import datetime as dt
 
-from server.database import Column, Model, SurrogatePK
+from server.database import Column, Model, SurrogatePK, relationship
 from server.database import db, reference_col
 from server.extensions import ma
-from server.users.models import UserSchema
+# from server.users.models import UserSchema
 from marshmallow import fields
 
 
@@ -14,21 +14,22 @@ class Extra(SurrogatePK, Model):
 
     __tablename__ = 'extras'
     user_id = reference_col('users', nullable=True)
-    vat_number = Column(db.String(11), unique=True, nullable=False)
-    insurance_number = Column(db.String(9), unique=True, nullable=False)
-    name = Column(db.String(80), nullable=False)
-    last_name = Column(db.String(255), nullable=False)
-    email = Column(db.String(80), unique=True, nullable=False)
-    birthday = Column(db.DateTime, nullable=False)
-    phone = Column(db.Integer(), nullable=False)
-    address = Column(db.String(200), nullable=False)
-    postal_code = Column(db.Integer(), nullable=False)
-    created_at = Column(db.DateTime, nullable=False,
+    user = relationship('User', backref='extras')
+    vat_number = Column(db.String(11), unique=True, nullable=True)
+    insurance_number = Column(db.String(20), unique=True, nullable=True)
+    name = Column(db.String(80), nullable=True)
+    last_name = Column(db.String(255), nullable=True)
+    email = Column(db.String(80), unique=True, nullable=True)
+    birthday = Column(db.DateTime, nullable=True)
+    phone = Column(db.Integer(), nullable=True)
+    address = Column(db.String(200), nullable=True)
+    postal_code = Column(db.Integer(), nullable=True)
+    created_at = Column(db.DateTime, nullable=True,
                         default=dt.datetime.utcnow)
 
     def __init__(self, vat_number, insurance_number, **kwargs):
         """Create instance."""
-        db.Model.__init(self, vat_number=vat_number,
+        db.Model.__init__(self, vat_number=vat_number,
                         insurance_number=insurance_number, **kwargs)
 
     def __repr__(self):
@@ -37,7 +38,7 @@ class Extra(SurrogatePK, Model):
 
 class ExtraSchema(ma.Schema):
     id = fields.Integer(dump_only=True)
-    user_id = fields.Nested(UserSchema, only=["user_id"])
+    # user_id = fields.Nested(UserSchema, only=["user_id"])
     vat_number = fields.String()
     insurance_number = fields.String()
     name = fields.String()
